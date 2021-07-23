2bd5b199a092bc66f7dffa570d5dcd6c345d51d0
vijaylaxmi26 <68533681+vijaylaxmi26@users.noreply.github.com>
2021-07-23 17:52:12 +0530

    lazy loading images

--- script.js
+++ script.js
@@ -182,6 +182,28 @@ allSection.forEach(function (section) {
   section.classList.add('section--hidden');
 });
 
+//lazy loading images
+const imgTarget = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+
+  //replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+});
+
+imgTarget.forEach(img => imgObserver.observe(img));
 //////////////////////////////
 //////////////////////////////
 //////////////////////////////
